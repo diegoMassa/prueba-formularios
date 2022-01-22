45cca3319f68d4cc3a0ad7d58bc412e0c8b50852
Diego Munoz <Diego.Munoz@endava.com>
2022-01-22 16:48:26 -0500

    Initial Commit

---
+++ src/main/java/com/prueba/formulario/PruebaTecnicaFormularioApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.formulario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaFormularioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaFormularioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/formulario/PruebaTecnicaFormularioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.formulario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaFormularioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
