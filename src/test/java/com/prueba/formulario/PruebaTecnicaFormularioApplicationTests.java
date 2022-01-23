36077ffd2f017b8bc04e1045996bbe1cf4589ba6
Diego Munoz <Diego.Munoz@endava.com>
2022-01-22 19:06:30 -0500

    adding a couple junit test

--- src/test/java/com/prueba/formulario/PruebaTecnicaFormularioApplicationTests.java
+++ src/test/java/com/prueba/formulario/PruebaTecnicaFormularioApplicationTests.java
@@ -1,13 +1,38 @@
 package com.prueba.formulario;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.prueba.formulario.dto.EncuestaRequestDTO;
+import com.prueba.formulario.dto.EncuestaResponseDTO;
+import com.prueba.formulario.services.IEncuesta;
 
 @SpringBootTest
 class PruebaTecnicaFormularioApplicationTests {
 
+	@Autowired
+	private IEncuesta encuestaService;
+	
 	@Test
-	void contextLoads() {
+	public void givenEncuestaRequest_whenSaveAndRetreiveEntity_thenOK() {
+		EncuestaResponseDTO genericResponseDTO = encuestaService.save(EncuestaRequestDTO.builder()
+				.numeroDocumento(1144040220L)
+				.email("abc@xyz.com")
+				.comentarios("N/A")
+				.idMarcaSeleccionada(1L)
+				.build());
+		assertNotNull(genericResponseDTO);
 	}
-
+	
+	
+	@Test
+	public void findAllForms_retreiveAllEntity_thenOK() {
+		List<EncuestaResponseDTO> listEncuesta = encuestaService.findAll();
+		assertNotNull(listEncuesta);
+	}
 }
